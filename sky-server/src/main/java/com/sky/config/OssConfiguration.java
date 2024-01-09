91213ccef59a7073f8860f6d416be1e6c1e028a8
xu482 <151128320+xu482@users.noreply.github.com>
2024-01-09 14:40:17 +0800

    外卖系统后端
    外卖系统后端

---
+++ sky-server/src/main/java/com/sky/config/OssConfiguration.java
@@ -0,0 +1,35 @@
+package com.sky.config;
+
+/**
+ * ClassName: OssConfiguration
+ * Package: com.sky.config
+ * Description:
+ *
+ * @Author 许佳辉
+ * @Create 2023/11/21 14:01
+ * @Version 1.0
+ */
+
+import com.sky.properties.AliOssProperties;
+import com.sky.utils.AliOssUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 配置类，用于创建AliOssUtil对象
+ */
+@Configuration
+@Slf4j
+public class OssConfiguration {
+
+    @Bean
+    @ConditionalOnMissingBean
+    public AliOssUtil aliOssUtil(AliOssProperties aliOssProperties){
+        log.info("开始创建阿里云文件上传工具类对象:{}",aliOssProperties);
+        return new AliOssUtil(aliOssProperties.getEndpoint(), aliOssProperties.getAccessKeyId(),
+                aliOssProperties.getAccessKeySecret(), aliOssProperties.getBucketName());
+
+    }
+}
---
+++ sky-server/src/main/java/com/sky/mapper/OrderDetailMapper.java
@@ -0,0 +1,29 @@
+package com.sky.mapper;
+
+import com.sky.entity.OrderDetail;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
+
+/**
+ * ClassName: OrderDetailMapping
+ * Package: com.sky.mapper
+ * Description:
+ *
+ * @Author 许佳辉
+ * @Create 2023/12/16 13:28
+ * @Version 1.0
+ */
+@Mapper
+public interface OrderDetailMapper {
+    void insertBatch(List<OrderDetail> orderDetailList);
+
+    /**
+     * 根据订单id查询订单明细
+     * @param orderId
+     * @return
+     */
+    @Select("select * from order_detail where order_id = #{orderId}")
+    List<OrderDetail> getByOrderId(Long orderId);
+}
